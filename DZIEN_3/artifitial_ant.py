86ac4c5bc94a78234928e24ce1261ac9905b8c4f
albim72 <marcin.albiniak72@gmail.com>
2023-06-15 15:18:20 +0200

    Update artifitial_ant.py

--- DZIEN_3/artifitial_ant.py
+++ DZIEN_3/artifitial_ant.py
@@ -28,13 +28,13 @@ class AntSimulator(object):
     direction = ["north","east","south","west"]
     dir_row = [1,0,-1,0]
     dir_col = [0,1,0,-1]
-    
+
     def __init__(self,max_moves):
         self.max_moves=max_moves
         self.moves=0
         self.eaten=0
         self.routine=None
-        
+
     def _reset(self):
         self.row = self.row_start
         self.col = self.col_start
@@ -42,9 +42,39 @@ class AntSimulator(object):
         self.moves=0
         self.eaten=0
         self.matrix_exc = copy.deepcopy(self.matrix)
-        
+
     @property
     def position(self):
         return (self.row,self.col,self.direction[self.dir])
+
+    def turn_left(self):
+        if self.moves < self.max_moves:
+            self.moves += 1
+            self.dir = (self.dir-1)%4
+            
+    def turn_right(self):
+        if self.moves < self.max_moves:
+            self.moves += 1
+            self.dir = (self.dir+1)%4
+            
+    def move_forward(self):
+        if self.moves < self.max_moves:
+            self.moves += 1
+            self.row = (self.row+self.dir_row[self.dir])%self.matrix_row
+            self.col = (self.col+self.dir_col[self.dir])%self.matrix_col
+            if self.matrix_exc[self.row][self.col] == "food":
+                self.eaten += 1
+            self.matrix_exc[self.row][self.col]="passed"
+            
+    def sense_food(self):
+        ahead_row = (self.row + self.dir_row[self.dir]) % self.matrix_row
+        ahead_col = (self.col + self.dir_col[self.dir]) % self.matrix_col
+        return self.matrix_exc[ahead_row][ahead_col] == "food"
     
+    def if_food_ahead(self,out1,out2):
+        return partial(if_then_else,self.sense_food,out1,out2)
     
+    def run(self,routine):
+        self._reset()
+        while self.moves < self.max_moves:
+            routine()
