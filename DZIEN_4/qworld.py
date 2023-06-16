38570ae51296b6336fef98c8e5d7a50bb47ad5fc
albim72 <marcin.albiniak72@gmail.com>
2023-06-16 10:14:39 +0200

    Update qworld.py

--- DZIEN_4/qworld.py
+++ DZIEN_4/qworld.py
@@ -35,15 +35,15 @@ class QWorld:
         #reset otoczenia
         self.reset()
         self.is_explore = True
-        
+
     def reset(self):
         """początek epizodu"""
         self.state = 0
         return self.state
-    
+
     def is_in_win_state(self):
         return self.state == 2
-    
+
     def init_reward_table(self):
         """
         # 0 - lewo, 1 - dół, 2 - prawo, 3 - góra
@@ -52,12 +52,12 @@ class QWorld:
         ___________________________
         |   0   |   0   |   -100  |
         ___________________________
-        
+
         """
         self.reward_table = np.zeros([self.row],[self.col])
         self.reward_table[1,2] = 100.
         self.reward_table[4,2] = -100.
-        
+
     def init_transition_table(self):
         """
         # 0 - lewo, 1 - dół, 2 - prawo, 3 - góra
@@ -69,7 +69,7 @@ class QWorld:
 
         """
         self.transition_table = np.zeros([self.row,self.col],dtype=int)
-        
+
         self.transition_table[0,0]= 0
         self.transition_table[0,1]= 3
         self.transition_table[0,2]= 1
@@ -79,7 +79,7 @@ class QWorld:
         self.transition_table[1, 1] = 4
         self.transition_table[1, 2] = 2
         self.transition_table[1, 3] = 1
-        
+
         #graniczny stan docelowy
 
         self.transition_table[2, 0] = 2
@@ -103,8 +103,27 @@ class QWorld:
         self.transition_table[5, 1] = 5
         self.transition_table[5, 2] = 5
         self.transition_table[5, 3] = 5
-    
 
+    def step(self,action):
+        """
+        wykonuje akcję na otoczeniu
+        Argument:
+        action(tensor): akcja w przestrzeni akcji
+        Zwraca:
+        next_state(tensor): następmny stan otoczenia
+        reward(float):nagroda dla agenta
+        done(Bool):czy osiągnięto cel graniczny
+        """
+        
+        #definicja następnego stanu (next_state), mając dany stan i akcję
+        next_state = self.transition_table[self.state,action]
+        #done ma wartość True, jeśsli next_state to cel albo dziura
+        done = next_state == 2 or next_state == 5
+        #nagroda dla stanu i akcji
+        reward = self.reward_table[self.state,action]
+        #otoczenie jest w nowy stanie
+        self.state = next_state
+        return next_state,reward,done
 
 
 
